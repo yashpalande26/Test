f76e5918c39267119783d7c1196216e7fe47a73d
yashpalande26 <125365662+yashpalande26@users.noreply.github.com>
2024-09-27 14:43:52 +0100

    day2.2

---
+++ Day2.2.js
@@ -0,0 +1,17 @@
+function summul(a,b,n){
+    let sum = 0;
+    for (let i = 0; i < n; i++){
+        if (i % a === 0 || i % b === 0){
+            sum = sum + i;
+        }
+    }
+    return sum
+}
+
+function summulip(){
+    let a = parseInt(document.getElementById("a").value);
+    let b = parseInt(document.getElementById("b").value);
+    let x = parseInt(document.getElementById("x").value);
+    let answer = summul(a,b,x;
+    alert("sum " + answer)
+}
