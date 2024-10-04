cfba2c3b015b2b1ea234fe7bf5764c1a21d65917
yashpalande26 <125365662+yashpalande26@users.noreply.github.com>
2024-10-04 14:12:54 +0100

    day4_array

---
+++ Day4_Array1.js
@@ -0,0 +1,23 @@
+// a.
+// Create a function which takes parameters a,b,l
+// l is a list of integers
+// find the sum of all the multiples of a or b in l
+
+function summul(a,b,n) {
+    let sum = 0
+
+    for(let i = 0; i < n.length; i++)
+    {
+        if(n[i] % a === 0 || n[i] % b === 0){
+            sum = sum + n[i]
+        }
+    }
+    return sum
+}
+
+let a = 3
+let b = 5
+let l = [2,6,88,22,76,13,17]
+
+
+console.log(summul(a,b,l))
---
+++ Day4_Array2.js
@@ -0,0 +1,24 @@
+// b.
+// Create a function which takes parameters a,l
+// a is a list of two integers
+// l is a list of integers
+// find the sum of all the multiples of elements of a in l
+
+
+function summul(a,b,n) {
+    let sum = 0
+
+    for(let i = 0; i < n.length; i++)
+    {
+        if(n[i] % a[0] === 0 || n[i] % a[1] === 0){
+            sum = sum + n[i]
+        }
+    }
+    return sum
+}
+
+let a = [3, 5];
+let l = [2,6,88,22,76,13,17]
+
+
+console.log(summul(a,l))
